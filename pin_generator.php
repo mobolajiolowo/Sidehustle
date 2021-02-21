b4ac970ef54bed6a13ba59a564f957a95c857c10
mobolajiolowo <67704493+mobolajiolowo@users.noreply.github.com>
2021-02-21 18:49:10 +0100

    Add files via upload

---
+++ pin_generator.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "Welcome to pin generator<br><br>";
+
+$i = 1;
+    while($i<=200)
+        {
+             echo  'Hello your recharge pin is ' . rand(100000000000,999999999999) . "<br>";
+         }
+        $i = $i + 1;
+
+
+
+
+
+?>
